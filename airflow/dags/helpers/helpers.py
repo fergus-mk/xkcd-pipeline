2a0759e35dc2503cba28ab2d7364bad04794fb8e
Fergus Miller Kerins <fergus.mk93@gmail.com>
2023-09-16 11:57:14 +0200

    added functions for single comic insert

--- airflow/dags/helpers/helpers.py
+++ airflow/dags/helpers/helpers.py
@@ -1,12 +1,22 @@
 import os
-import requests # Install
-from datetime import datetime, timedelta
 import psycopg2
+import requests
+from datetime import datetime, timedelta
 from decouple import config
+from helpers.sql_scripts import ComicSQLScripts
 
-# Connection
+# db conncect functions
 def connect_db(host: str, dbname: str, user: str, password: str, port: int):
-    """Connects to db using config variables from .env file"""
+    """
+    Establish a connection to the PostgreSQL database using environment variables.
+
+    :param host: Environment variable key for database host.
+    :param dbname: Environment variable key for database name.
+    :param user: Environment variable key for database user.
+    :param password: Environment variable key for database password.
+    :param port: Environment variable key for database port.
+    :return: psycopg2 database connection object.
+    """
     return psycopg2.connect(
         host=os.environ.get(host),
         database=os.environ.get(dbname),
@@ -15,65 +25,90 @@ def connect_db(host: str, dbname: str, user: str, password: str, port: int):
         port=os.environ.get(port)
     )
 
-# Accesing xkcd metadata
+# Populate comic functions
 def get_comic_info(comic_number=None) -> dict:
-    """If comic_number is None, fetch the current comic, otherwise fetch the comic with comic_number"""
+    """
+    Fetch metadata for a specific xkcd comic. If comic_number isn't provided, fetch the most recent comic.
+
+    :param comic_number: Comic issue number to fetch, or None for the latest comic.
+    :return: Dictionary containing comic metadata.
+    """
     url = f'https://xkcd.com/{comic_number if comic_number else ""}/info.0.json'
-
     response = requests.get(url)
-
     response.raise_for_status()  # Raises an exception if the GET request was not successful
+    return response.json()  # Parse the JSON response
 
-    json_response = response.json()  # Parse the JSON response
+def extract_date_from_comic(comic_dict: dict) -> datetime:
+    """
+    Extract the publication date of a comic from its metadata dictionary.
 
-    return json_response
-
-def extract_date_from_comic(comic_dict: dict)-> datetime:
-    """Extracts the datetime from comic dictionary"""
+    :param comic_dict: Dictionary containing comic metadata.
+    :return: Datetime object representing the comic's publication date.
+    """
     year = int(comic_dict['year'])
     month = int(comic_dict['month'])
     day = int(comic_dict['day'])
-    
     return datetime(year, month, day)
 
-def get_comic_issue_no(comic_dict: dict)-> int:
-    """Gets the issue number of input comic"""
+def get_comic_issue_no(comic_dict: dict) -> int:
+    """
+    Extract the issue number of a comic from its metadata dictionary.
+
+    :param comic_dict: Dictionary containing comic metadata.
+    :return: Integer representing the comic's issue number.
+    """
     return comic_dict['num']
 
-def date_compare(date1: datetime, date2: datetime)-> bool:
-    """Returns True if date1 is after date2, otherwise returns False"""
-    if date1 > date2:
-        return True
-    else:
-        return False
+def date_compare(date1: datetime, date2: datetime) -> bool:
+    """
+    Compare two dates to determine if the first date is after the second date.
 
-def date_x_days_ago(x: int):
-    """This function calculates date x days ago from current date, returns datetime object"""
-    current_date = datetime.now()
-    return current_date - timedelta(days=x)
+    :param date1: The first datetime object.
+    :param date2: The second datetime object.
+    :return: True if date1 is after date2, otherwise False.
+    """
+    return date1 > date2
 
-def inital_populate_comic_list(no_of_days: int):
-    """Create a list of comic JSONs from the last X days"""
+def date_x_days_ago(x: int) -> datetime:
+    """
+    Calculate the date x days ago from the current date.
+
+    :param x: Number of days to subtract from the current date.
+    :return: Datetime object representing the date x days ago.
+    """
+    return datetime.now() - timedelta(days=x)
+
+def inital_populate_comic_list(no_of_days: int) -> list:
+    """
+    Retrieve a list of comics from xkcd that were published within the last no_of_days.
+
+    :param no_of_days: Number of days to look back.
+    :return: List of dictionaries, each containing metadata for a comic.
+    """
     comics_list = []
-    
     most_recent_comic = get_comic_info()
     comic_issue_no = get_comic_issue_no(most_recent_comic)
 
     while True:
         comic = get_comic_info(comic_issue_no)
         comic_date = extract_date_from_comic(comic)
-        max_back_date = date_x_days_ago(no_of_days)  # The furthest date back to get comics
+        max_back_date = date_x_days_ago(no_of_days)
 
-        if not date_compare(comic_date, max_back_date):  # Check if comic within timeframe
-            break 
+        if not date_compare(comic_date, max_back_date):
+            break
 
         comics_list.append(comic)
         comic_issue_no -= 1
 
     return comics_list
 
-def convert_comic_list_to_tuples(comics_list: list) ->list:
-    """Takes a lits of comic dictionaries and converts into a list of tuples"""
+def convert_comic_list_to_tuples(comics_list: list) -> list:
+    """
+    Convert a list of comic metadata dictionaries into a list of tuples suitable for database insertion.
+
+    :param comics_list: List of dictionaries, each containing metadata for a comic.
+    :return: List of tuples, each representing a comic's metadata.
+    """
     return [
         (comic["month"], comic["num"], comic["link"], comic["year"],
         comic["news"], comic["safe_title"], comic["transcript"], comic["alt"],
@@ -81,6 +116,52 @@ def convert_comic_list_to_tuples(comics_list: list) ->list:
         for comic in comics_list
     ]
 
+def comic_dict_to_tuple(comic_dict: dict) -> tuple:
+    """
+    Convert a single comic metadata dictionary into a tuple suitable for database insertion.
+
+    :param comic_dict: Dictionary containing comic metadata.
+    :return: Tuple representing the comic's metadata.
+    """
+    return (
+        comic_dict['month'], comic_dict['num'], comic_dict['link'],
+        comic_dict['year'], comic_dict['news'], comic_dict['safe_title'],
+        comic_dict['transcript'], comic_dict['alt'], comic_dict['img'],
+        comic_dict['title'], comic_dict['day']
+    )
+
+def check_if_comic_in_db(conn, comic_num: int) -> bool:
+    """
+    Check if a comic with a specific issue number is already present in the database.
+
+    :param conn: Active database connection.
+    :param comic_num: Comic issue number to check.
+    :return: True if the comic is in the database, otherwise False.
+    """
+    comic_details = execute_sql(
+        query=ComicSQLScripts.select_comic_by_num,
+        params=(comic_num,),
+        connection=conn
+    )
+    return bool(comic_details)
+
+def add_most_recent_comic(conn):
+    """
+    Fetch the most recent comic from xkcd and add it to the database if it's not already present.
+
+    :param conn: Active database connection.
+    :return: None
+    """
+    current_comic = get_comic_info()
+    current_comic_num = current_comic['num']
+    current_comic_tuple = comic_dict_to_tuple(current_comic)
+
+    if check_if_comic_in_db(conn, current_comic_num):
+        print(f"comic with num {current_comic_num} already in db") # Visible in ariflow logs
+    else:
+        execute_sql(ComicSQLScripts.insert_comic, current_comic_tuple, conn)
+
+# Execute SQL scripts functions
 def execute_batch_sql(query, params_list, connection):
     """
     Execute a batch SQL insert.
@@ -97,3 +178,32 @@ def execute_batch_sql(query, params_list, connection):
     except Exception as e:
         print(f"An error occurred: {e}")
         raise
+
+def execute_sql(query, params, connection):
+    """
+    Execute a single SQL statement.
+
+    :param query: The SQL query string.
+    :param params: A tuple containing parameters for the SQL query.
+    :param connection: Active database connection.
+    :return: Results if it's a SELECT query, otherwise None
+    """
+    results = None
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, params)
+
+            # If the query is a SELECT statement, fetch the results
+            if query.strip().upper().startswith("SELECT"):
+                results = cursor.fetchall()
+
+            # Otherwise, commit the changes
+            else:
+                connection.commit()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise
+
+    return results
